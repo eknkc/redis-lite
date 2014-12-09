c361f91a2fcc72d92316837f9c6ca40beb6cae43
namirali <n.amirali@gmail.com>
2014-12-09 15:20:44 +0200

    rename test fix

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,5 @@
 var redis = require("../");
 
-module.exports.client = function  () {
-  return redis('localhost');
+module.exports.client = function () {
+  return redis(['localhost:6379', 'localhost:8888', 'localhost:9999'], { removeTimeout: 1000 });
 }
