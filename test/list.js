66587b7b8f48a3d7d470579259cfe34b41fcc6b2
namirali <n.amirali@gmail.com>
2014-12-10 19:42:02 +0200

    rpushx lpush test

--- test/list.js
+++ test/list.js
@@ -87,6 +87,166 @@ describe('Lists', function () {
     })
   })
 
+  it('RPUSHX: should insert value in list if key exits', function (done) {
+    var key = crypto.randomBytes(8).toString('hex')
+      , val1 = crypto.randomBytes(8).toString('hex')
+      , val2 = crypto.randomBytes(8).toString('hex');
+
+    async.series({
+      check: function (next) {
+        c.exists(key, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 0, 'should return 0 if not exists');
+
+          next();
+        })
+      },
+      rpush: function (next) {
+        c.rpush(key, val1, function (err, data) {
+          assert.ok(!err);
+          assert.ok(data > 0, 'should return len of list');
+
+          next();
+        })
+      },
+      rpushx: function (next) {
+        c.rpushx(key, val2, function (err, data) {
+          assert.ok(!err);
+          assert.ok(data > 0, 'should return len of list');
+
+          next();
+        })
+      },
+      list: function (next) {
+        c.lrange(key, 0, 100, function (err, data) {
+          assert.ok(!err);
+          assert.equal(typeof data, 'object', 'should return obj');
+          assert.equal(data[1], val2, 'should be same with val2');
+
+          next();
+        })
+      }
+    }, function () {
+      done();
+    })
+  })
+
+  it('RPUSHX: should fail to insert value in list if key is not exit', function (done) {
+    var key = crypto.randomBytes(8).toString('hex')
+      , val1 = crypto.randomBytes(8).toString('hex')
+      , val2 = crypto.randomBytes(8).toString('hex');
+
+    async.series({
+      check: function (next) {
+        c.exists(key, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 0, 'should return 0 if not exists');
+
+          next();
+        })
+      },
+      rpushx: function (next) {
+        c.rpushx(key, val2, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 0, 'should return 0 if not st');
+
+          next();
+        })
+      },
+      list: function (next) {
+        c.lrange(key, 0, 100, function (err, data) {
+          assert.ok(!err);
+          assert.equal(typeof data, 'object', 'should return obj');
+          assert.equal(data.length, 0, 'should return 0 as array lenght');
+
+          next();
+        })
+      }
+    }, function () {
+      done();
+    })
+  })
+
+  it('LPUSHX: should insert value in list if key exits', function (done) {
+    var key = crypto.randomBytes(8).toString('hex')
+      , val1 = crypto.randomBytes(8).toString('hex')
+      , val2 = crypto.randomBytes(8).toString('hex');
+
+    async.series({
+      check: function (next) {
+        c.exists(key, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 0, 'should return 0 if not exists');
+
+          next();
+        })
+      },
+      rpush: function (next) {
+        c.rpush(key, val1, function (err, data) {
+          assert.ok(!err);
+          assert.ok(data > 0, 'should return len of list');
+
+          next();
+        })
+      },
+      rpushx: function (next) {
+        c.lpushx(key, val2, function (err, data) {
+          assert.ok(!err);
+          assert.ok(data > 0, 'should return len of list');
+
+          next();
+        })
+      },
+      list: function (next) {
+        c.lrange(key, 0, 100, function (err, data) {
+          assert.ok(!err);
+          assert.equal(typeof data, 'object', 'should return obj');
+          assert.equal(data[0], val2, 'should be same with val2');
+
+          next();
+        })
+      }
+    }, function () {
+      done();
+    })
+  })
+
+  it('LPUSHX: should fail to insert value in list if key is not exit', function (done) {
+    var key = crypto.randomBytes(8).toString('hex')
+      , val1 = crypto.randomBytes(8).toString('hex')
+      , val2 = crypto.randomBytes(8).toString('hex');
+
+    async.series({
+      check: function (next) {
+        c.exists(key, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 0, 'should return 0 if not exists');
+
+          next();
+        })
+      },
+      rpushx: function (next) {
+        c.lpushx(key, val2, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 0, 'should return 0 if not st');
+
+          next();
+        })
+      },
+      list: function (next) {
+        c.lrange(key, 0, 100, function (err, data) {
+          assert.ok(!err);
+          assert.equal(typeof data, 'object', 'should return obj');
+          assert.equal(data.length, 0, 'should return 0 as array lenght');
+
+          next();
+        })
+      }
+    }, function () {
+      done();
+    })
+  })
+
   it('RPOP: should remove the last element in list', function (done) {
     var key = crypto.randomBytes(8).toString('hex')
       , val1 = crypto.randomBytes(8).toString('hex')
