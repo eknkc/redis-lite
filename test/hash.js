86dbceafe145ef5e07dacb5405aa3aa046ea67c1
namirali <n.amirali@gmail.com>
2014-12-10 13:07:39 +0200

    hset test

--- test/hash.js
+++ test/hash.js
@@ -1,7 +1,42 @@
 var assert = require('assert')
   , async = require('async')
-  , crypto = require('crypto');
+  , crypto = require('crypto')
+  , helper = require('./helpers.js');
 
 describe('Hashes', function () {
+  var c = helper.client();
+
+  it('HSET: should set', function (done) {
+    var key = crypto.randomBytes(8).toString('hex');
+    c.hset(key, key, key, function (err, data) {
+      assert.ok(!err);
+      assert.equal(data, 1, 'should return 1 if set');
+
+      done();
+    })
+  });
+
+  it('HSET: should fail to set existing field', function (done) {
+    var key = crypto.randomBytes(8).toString('hex');
+
+    async.series({
+      hset: function (next) {
+        c.hset(key, key, key, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 1, 'should return 1 if set');
+
+          done();
+        })
+      },
+      hsetAgain: function (next) {
+        c.hset(key, key, key, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 0, 'should return 0 if set');
+
+          done();
+        })
+      }
+    })
+  });
 
 });
