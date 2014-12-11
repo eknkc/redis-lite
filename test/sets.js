85f759aedf4faa02e56aa057274abc4cb29f4496
namirali <n.amirali@gmail.com>
2014-12-11 16:42:05 +0200

    sdiff test

--- test/sets.js
+++ test/sets.js
@@ -87,4 +87,82 @@ describe('Sets', function () {
     })
   })
 
+  it('SDIFF: should return differences between keys', function (done) {
+    var key1 = '{deneme123}' + crypto.randomBytes(8).toString('hex')
+      , key2 = '{deneme123}' + crypto.randomBytes(8).toString('hex')
+      , val1 = crypto.randomBytes(8).toString('hex')
+      , val2 = crypto.randomBytes(8).toString('hex')
+      , val3 = crypto.randomBytes(8).toString('hex')
+      , val4 = crypto.randomBytes(8).toString('hex');
+
+    async.series({
+      sadd1: function (next) {
+        c.sadd(key1, val1, val2, val3, val4, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 4, 'should return 4 as len of key');
+
+          next();
+        })
+      },
+      sadd2: function (next) {
+        c.sadd(key2, val1, val2, val3, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 3, 'should return 3 as len of key');
+
+          next();
+        })
+      },
+      sdiff: function (next) {
+        c.sdiff(key1, key2, function (err, data) {
+          assert.ok(!err);
+          assert.equal(typeof data, 'object', ' should retun obj');
+          assert.equal(data[0], val4, 'should return val4 as diff');
+
+          next();
+        })
+      }
+    }, function () {
+      done();
+    })
+
+  });
+
+  it('SDIFF: should fail to return differences between keys from different hash ranges', function (done) {
+    var key1 = '{aganiginaganigi}' + crypto.randomBytes(8).toString('hex')
+      , key2 = '{deneme123}' + crypto.randomBytes(8).toString('hex')
+      , val1 = crypto.randomBytes(8).toString('hex')
+      , val2 = crypto.randomBytes(8).toString('hex')
+      , val3 = crypto.randomBytes(8).toString('hex')
+      , val4 = crypto.randomBytes(8).toString('hex');
+
+    async.series({
+      sadd1: function (next) {
+        c.sadd(key1, val1, val2, val3, val4, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 4, 'should return 4 as len of key');
+
+          next();
+        })
+      },
+      sadd2: function (next) {
+        c.sadd(key2, val1, val2, val3, function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 3, 'should return 3 as len of key');
+
+          next();
+        })
+      },
+      sdiff: function (next) {
+        c.sdiff(key1, key2, function (err, data) {
+          assert.ok(err);
+
+          next();
+        })
+      }
+    }, function () {
+      done();
+    })
+
+  })
+
 });
