2d2117c2e1ca15da0a59d32ba72235a59abd807b
namirali <n.amirali@gmail.com>
2014-12-11 15:58:02 +0200

    sets test init

---
+++ test/sets.js
@@ -0,0 +1,10 @@
+var assert = require('assert')
+  , async = require('async')
+  , crypto = require('crypto')
+  , helper = require('./helpers.js');
+
+
+describe('Sets', function () {
+  var c = helper.client();
+
+});
