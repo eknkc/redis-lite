b4abe47f0f228140b90f6d263d8def7821601676
namirali <n.amirali@gmail.com>
2014-12-11 16:09:29 +0200

    sadd test

--- test/sets.js
+++ test/sets.js
@@ -7,4 +7,30 @@ var assert = require('assert')
 describe('Sets', function () {
   var c = helper.client();
 
+  it('SADD: should add some members in a set ', function (done) {
+    var key = crypto.randomBytes(4).toString('hex');
+
+    async.series({
+      sadd: function (next) {
+        c.sadd(key, crypto.randomBytes(4).toString('hex'), crypto.randomBytes(4).toString('hex'), crypto.randomBytes(4).toString('hex'), function (err, data) {
+          assert.ok(!err);
+          assert.equal(data, 3, 'should return 3 as length of insert');
+
+          next();
+        })
+      },
+      smembers: function (next) {
+        c.smembers(key, function (err, data) {
+          assert.ok(!err);
+          assert.equal(typeof  data, 'object', 'should return obj');
+          assert.equal(data.length, 3, 'should return 3 as length of set');
+
+          next();
+        })
+      }
+    }, function () {
+      done();
+    })
+  })
+
 });
